eecadc01fbd3027408285e7b16644238333488e5
Kevin-RB <69731479+Kevin-RB@users.noreply.github.com>
2021-03-07 16:54:19 -0500

    Fixed warnings

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,21 +1,17 @@
 import React, {useState} from 'react';
 import './Button.css';
 import { withRouter } from "react-router";
-// import {useState} from 'React';
-// Botón general para las cards de agregar productos 
 
-function Button(props){
+function Button(){
     const [btnText, setBtnT] = useState("AGREGAR");
     const changeT = (text) => {
 
-        if (btnText=="AGREGAR"){
+        if (btnText==="AGREGAR"){
             setBtnT(text);
         }else{
             setBtnT("AGREGAR");
         }
     }
-
-    // let btnText = props.btnText;
     return (
         <div>
             <button className="btnQuick" onClick={() => changeT("AGREGADO")} >{btnText}</button>
--- src/MainCart/MainCart.js
+++ src/MainCart/MainCart.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Modal from 'react-modal';
 import  Navbar  from '../Components/Navbar/Navbar';
 import img from '../assets/img/bochalema.jpg';
